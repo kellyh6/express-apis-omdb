36db2bb71ea2be9819c91e7d91c7e8cf4023566e
kellyh6 <faithkeeping@yahoo.com>
2017-02-27 19:23:32 -0800

    project status

---
+++ controllers/movieRoute.js
@@ -0,0 +1,11 @@
+// Requires & Global Vars
+// var express = require('express');
+// var request = require('request');
+// var router = express.Router();
+
+// var request = require('request');
+// request('http://www.omdbapi.com/', function (error, response, body) {
+//   if (!error && response.statusCode == 200) {
+//     console.log(body) // Show the HTML for the Google homepage.
+//   }
+// });
--- index.js
+++ index.js
@@ -1,12 +1,26 @@
+//Requires & Global Vars
 var express = require('express');
+var bodyParser = require('body-parser');
+var ejsLayouts = require('express-ejs-layouts');
 var app = express();
 
-// app.use(require('morgan')('dev'));
+//Set & Use Statements
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(ejsLayouts);
+app.use(express.static(__dirname + "/public"));
 
+
+//Routes
 app.get('/', function(req, res) {
-  res.send('Hello Backend!');
+  // res.render('main.ejs');
+  console.log(Hello backend);
 });
 
+//Include external routes
+app.use('/', require('./controllers/movieRoute'));
+
+//Listen
 var server = app.listen(process.env.PORT || 3000);
 
 module.exports = server;
